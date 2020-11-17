1cfcd3c2174c3c697fbdd5754b03672e10a7e0b8
Er-Gray <sakuya.mybride@gmail.com>
2020-11-17 19:28:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,267 @@
-import logo from './logo.svg';
+import React, { useState, createContext, useContext, useEffect } from "react";
+import { useForm } from 'react-hook-form';
+import ViewArea from "./ViewArea";
 import './App.css';
 
+const Context = createContext();
 function App() {
+  const oneDSix = () => { return Math.floor(Math.random() * (6 - 1)) + 1 };
+
+  const [race, setRace] = useState({ "name": "", "generalSkill": ["", ""], "adventureSkill": ["", ""] });
+  const [stateValues, setStateValues] = useState(Array(3).fill(0).map(() => { return oneDSix() + oneDSix() }));
+  const [firstStatus, setFirstStatus] = useState({ "体力点": 0, "魂魄点": 0, "技量点": 0, "知力点": 0 });
+  const [secondStatus, setSecondStatus] = useState({ "集中度": 0, "持久度": 0, "反射度": 0 });
+  const [histories, setHistories] = useState({ "出自": "", "来歴": "", "邂逅": "" });
+  const [status, setStatus] = useState({ "生命力": 0, "移動力": 0, "呪文回数": undefined });
+  const [coin, setCoin] = useState(100);
+  console.log(histories);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Context.Provider value={{ race: race, firstStatus: firstStatus, secondStatus: secondStatus, histories: histories, status: status, coin: coin }}>
+        <ViewArea />
+      </Context.Provider>
+      <div className="stateValueWrapper">
+        <p>現在の状態値の出目:{stateValues.join()}</p>
+      </div>
+      <Context.Provider value={{
+        race: race,
+        setRace: setRace,
+        firstStatus: firstStatus,
+        setFirstStatus: setFirstStatus,
+        secondStatus: secondStatus,
+        setSecondStatus: setSecondStatus,
+        histories: histories,
+        setHistories: setHistories,
+        stateValues: stateValues,
+        setStateValues: setStateValues,
+        oneDSix: oneDSix,
+        status: status,
+        setStatus: setStatus,
+        coin: coin,
+        setCoin: setCoin
+      }}>
+        <InputArea />
+      </Context.Provider>
+    </div>
+  );
+}
+
+function InputArea() {
+  const races = require("./race.json");
+  const table = require("./table.json");
+  const statusTable = require("./status.json");
+  const { register, handleSubmit, errors } = useForm();
+
+  const oneDThree = () => { return Math.floor(Math.random() * (3 - 1)) + 1 };
+
+  const raceChoices = () => {
+    return Object.keys(races.種族).map((viewRace, index) => <option key={index} value={viewRace}>{viewRace}</option>);
+  };
+
+  const StatusChoice = (status) => {
+    return Object.keys(status).map((key, index) => (<option key={index} value={key}>{key}</option>));
+  };
+
+  const valueChoice = () => {
+    return stateValues.map((value, index) => <option key={index} value={value}>{value}</option>);
+  }
+
+  const sumStatus = () => {
+    let sum = 0;
+    Object.keys(firstStatus).forEach((key) => { sum += firstStatus[key] });
+    Object.keys(secondStatus).forEach((key) => { sum += secondStatus[key] });
+    return sum;
+  };
+
+  const raceSubmit = (data) => {
+    setRace(races.種族[data.decided]);
+    setRandomOrFixed(data.randomOrFixed);
+    console.log(errors)
+  };
+
+  const reliefSubmit = (data) => {
+    if (firstStatus[data.decided]) {
+      setFirstStatus({ ...firstStatus, [data.decided]: 3 + statusTable.ランダム修正[race.name][0][data.decided] });
+    } else if (secondStatus[data.decided]) {
+      setSecondStatus({ ...secondStatus, [data.decided]: 3 + statusTable.ランダム修正[race.name][1][data.decided] });
+    }
+    setStatusReliefed(true);
+  };
+
+  const bonusSubmit = (data) => {
+    setFirstStatus({ ...firstStatus, [data.decided]: firstStatus[data.decided] + 1 });
+    setBonusAdded(true);
+  };
+
+  const stateValueSubmit = (data) => {
+    if (data.decided === "yes") {
+      setStateValues([5, 7, 9]);
+    }
+    setStateFixed(true);
+  };
+
+  const vitalitySubmit = (data) => {
+    console.log(data);
+    if (data.decided) {
+      setStatus({ ...status, "生命力": parseInt(data.decided) + firstStatus.体力点 + firstStatus.魂魄点 + secondStatus.持久度 });
+      const newValues = [...stateValues];
+      newValues.splice(newValues.findIndex((element) => element === parseInt(data.decided)), 1);
+      setStateValues(newValues);
+    } else {
+      return (<p>値を入力してください</p>);
+    }
+  };
+
+  const moveSubmit = (data) => {
+    if (data.decided) {
+      console.log(race.move);
+      setStatus({ ...status, "移動力": parseInt(data.decided) * race.move });
+      const newValues = [...stateValues];
+      newValues.splice(newValues.findIndex((element) => element === parseInt(data.decided)), 1);
+      setStateValues(newValues);
+    } else {
+      return (<p>値を入力してください</p>);
+    };
+  };
+
+  const decideSpellCount = () => {
+    let spellCount;
+    if (stateValues[0] >= 12) {
+      spellCount = 3;
+    } else if (stateValues[0] >= 10) {
+      spellCount = 2;
+    } else if (stateValues[0] >= 7) {
+      spellCount = 1;
+    } else {
+      spellCount = 0;
+    }
+    setStatus({ ...status, "呪文回数": spellCount });
+    const newValues = [...stateValues];
+    newValues.splice(newValues.findIndex((element) => element === parseInt(stateValues[0])), 1);
+    setStateValues(newValues);
+  };
+
+  const { race, setRace, firstStatus, setFirstStatus, secondStatus, setSecondStatus, histories, setHistories, stateValues, setStateValues, oneDSix, status, setStatus, coin, setCoin } = useContext(Context);
+  const [randomOrFixed, setRandomOrFixed] = useState("");
+
+  let inputArea;
+  const [statusReliefed, setStatusReliefed] = useState(false);
+  const [bonusAdded, setBonusAdded] = useState(false);
+  const [stateFixed, setStateFixed] = useState(false);
+  const [addedCoin, setAddedCoin] = useState(false);
+
+  useEffect(() => {
+    if (race.name !== "") {
+      if (randomOrFixed === "fixed") {
+        setFirstStatus({ "体力点": statusTable.固定値[race.name][0].体力点, "魂魄点": statusTable.固定値[race.name][0].魂魄点, "技量点": statusTable.固定値[race.name][0].技量点, "知力点": statusTable.固定値[race.name][0].知力点 });
+        setSecondStatus({ "集中度": statusTable.固定値[race.name][1].集中度, "持久度": statusTable.固定値[race.name][1].持久度, "反射度": statusTable.固定値[race.name][1].反射度 });
+        setRandomOrFixed("");
+      } else if (randomOrFixed === "random") {
+        setFirstStatus({ "体力点": oneDThree() + statusTable.ランダム修正[race.name][0].体力点, "魂魄点": oneDThree() + statusTable.ランダム修正[race.name][0].魂魄点, "技量点": oneDThree() + statusTable.ランダム修正[race.name][0].技量点, "知力点": oneDThree() + statusTable.ランダム修正[race.name][0].知力点 });
+        setSecondStatus({ "集中度": oneDThree() + statusTable.ランダム修正[race.name][1].集中度, "持久度": oneDThree() + statusTable.ランダム修正[race.name][1].持久度, "反射度": oneDThree() + statusTable.ランダム修正[race.name][1].反射度 });
+        setRandomOrFixed("");
+      }
+
+      if (histories.出自 === "" && histories.来歴 === "" && histories.邂逅 === "") {
+        setHistories({ "出自": table.出自[race.name][oneDSix() + oneDSix() - 2], "来歴": table.来歴[oneDSix() + oneDSix() - 2], "邂逅": table.邂逅[oneDSix() + oneDSix() - 2] });
+
+      }
+
+      if (histories.出自.coin && !addedCoin) {
+        setCoin(coin + (oneDSix() + oneDSix()) * histories.出自.coin);
+        setAddedCoin(true);
+      }
+
+    }
+
+
+  }, [race, randomOrFixed, setFirstStatus, setSecondStatus, statusTable.固定値, statusTable.ランダム修正, histories, setHistories, oneDSix, table, coin, setCoin, addedCoin]);
+
+  if (race.name === "") {
+    inputArea = (
+      <form onSubmit={handleSubmit(raceSubmit)}>
+        <p>種族と能力値をランダムにするか固定値にするか選択してください</p>
+        <select name="decided" defaultValue={races.種族.只人.name} ref={register}>
+          {raceChoices()}
+        </select>
+        <select name="randomOrFixed" defaultValue="random" ref={register}>
+          <option value="random">ランダム</option>
+          <option value="fixed">固定値</option>
+        </select>
+        <button type="submit">決定</button>
+      </form>
+    );
+  } else if (sumStatus() <= 15 && !statusReliefed) {
+    inputArea = (
+      <form onSubmit={handleSubmit(reliefSubmit)}>
+        <p>能力値合計が15以下なので好きな能力値の出目を3がでたことにできます</p>
+        <select name="decided" defaultValue="使わない" ref={register}>
+          <option value="使わない">使わない</option>
+          {StatusChoice(firstStatus)}
+          {StatusChoice(secondStatus)}
+        </select>
+        <button type="submit">決定</button>
+      </form>
+    );
+  } else if (!bonusAdded) {
+    inputArea = (
+      <form onSubmit={handleSubmit(bonusSubmit)}>
+        <p>ボーナスを加算する第一能力値を選択してください</p>
+        <select name="decided" defaultValue="体力点" ref={register}>
+          {StatusChoice(firstStatus)}
+        </select>
+        <button type="submit">決定</button>
+      </form>
+    );
+  } else if (!stateFixed) {
+    inputArea = (
+      <form onSubmit={handleSubmit(stateValueSubmit)}>
+        <p>状態値に固定値を使いますか？</p>
+        <select name="decided" defaultValue="no" ref={register}>
+          <option value="no">いいえ</option>
+          <option value="yes">はい</option>
+        </select>
+        <button type="submit">決定</button>
+      </form>
+    );
+  } else if (status.生命力 === 0) {
+    inputArea = (
+      <form onSubmit={handleSubmit(vitalitySubmit)}>
+        <p>生命力に使う出目を選んでください</p>
+        <select name="decided" ref={register}>
+          <option value={null}> </option>
+          {valueChoice()}
+        </select>
+        <button type="submit">決定</button>
+      </form>
+    );
+  } else if (status.移動力 === 0) {
+    inputArea = (
+      <form onSubmit={handleSubmit(moveSubmit)}>
+        <p>移動力に使う出目を選んでください</p>
+        <select name="decided" ref={register}>
+          <option value={null}> </option>
+          {valueChoice()}
+        </select>
+        <button type="submit">決定</button>
+      </form>
+    );
+  } else if (stateValues[0]) {
+    decideSpellCount();
+  } else {
+    inputArea = (
+      <p>キャラシ完成です</p>
+    );
+  }
+
+
+
+  return (
+    <div>
+      {inputArea}
     </div>
   );
 }
 
 export default App;
+export { Context };
---
+++ src/ViewArea.js
@@ -0,0 +1,107 @@
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "./App";
+
+function ViewArea() {
+    const { race, firstStatus, secondStatus, histories, status, coin } = useContext(Context);
+    const [value, setValue] = useState("");
+
+    useEffect(() => {
+        setValue(
+            `名前：【】　　種族：【 ${race.name ? race.name : ""} 】　　性別：【 】　　年齢：【  】　　PL：【  】
+                
+経歴：【  ${histories.出自.name ? histories.出自.name : ""}／ ${histories.来歴.name ? histories.来歴.name : ""} ／ ${histories.邂逅} 】　　等級：【 白磁等級 】
+    
+身体的特徴：【  】
+    
+経験点：【 3000 ／ 3000 】　　成長点：【 10 ／ 10 】
+    
+冒険回数：【 0 】　　冒険達成数：【 0 】
+
+◆能力値
+【能力値】                           [第二能力値]
+［第一能力値］   【 集中度：${secondStatus.集中度} 】    【 持久度：${secondStatus.持久度} 】    【 反射度：${secondStatus.反射度} 】
+【 体力点：${firstStatus.体力点} 】          [ ${firstStatus.体力点 + secondStatus.集中度} ]                   [ ${firstStatus.体力点 + secondStatus.持久度} ]                   [ ${firstStatus.体力点 + secondStatus.反射度} ]
+【 魂魄点：${firstStatus.魂魄点} 】          [ ${firstStatus.魂魄点 + secondStatus.集中度} ]                   [ ${firstStatus.魂魄点 + secondStatus.持久度} ]                   [ ${firstStatus.魂魄点 + secondStatus.反射度} ]
+【 技量点：${firstStatus.技量点} 】          [ ${firstStatus.技量点 + secondStatus.集中度} ]                   [ ${firstStatus.技量点 + secondStatus.持久度} ]                   [ ${firstStatus.技量点 + secondStatus.反射度} ]
+【 知力点：${firstStatus.知力点} 】          [ ${firstStatus.知力点 + secondStatus.集中度} ]                   [ ${firstStatus.知力点 + secondStatus.持久度} ]                   [ ${firstStatus.知力点 + secondStatus.反射度} ]
+
+生命力：【 ${status.生命力} 】　 　 生命力２倍：【 ${status.生命力 * 2} 】
+
+移動力：【 ${status.移動力} 】　　　呪文使用回数：【 ${status.呪文回数 ? status.呪文回数 : ""} 】
+
+呪文抵抗基準値（魂魄反射+冒険者LV+技能補正）：【 ${firstStatus.魂魄点 + secondStatus.反射度 + 1} 】
+
+◆冒険者レベル：【 1 】
+職業レベル：【 ${histories.出自.profession ? histories.出自.profession : ""}：${histories.出自.profession ? 1 : ""} 】【 ： 】
+
+◆冒険者技能　　初歩　／　習熟　／　熟達　／　達人　／　伝説　／　効果
+${race.adventureSkill[0] ? `【${race.adventureSkill[0]}】　 ●　　　　○　　　　○　　　　○　　 　 ○　 ／\n` : ""}${race.adventureSkill[1] ? `【${race.adventureSkill[1]}】　 ●　　　　○　　　　○　　　　○　　 　 ○　 ／\n` : ""}${histories.出自.adventureSkill ? `【${histories.出自.adventureSkill}】　 ●　　　　○　　　　○　　　　○　　 　 ○　 ／\n` : ""}${histories.来歴.adventureSkill ? `【${histories.来歴.adventureSkill}】　 ●　　　　○　　　　○　　　　○　　 　 ○　 ／` : ""}
+
+◆一般技能 　 　初歩　／　習熟　／　熟達　／　達人　／　伝説　／　効果
+${race.generalSkill[0] ? `【${race.generalSkill[0]}】　 ●　　　　○　　　　○　　　　○　　 　 ○　 ／\n` : ""}${race.generalSkill[1] ? `【${race.generalSkill[1]}】　 ●　　　　○　　　　○　　　　○　　 　 ○　 ／\n` : ""}${histories.出自.generalSkill ? `【${histories.出自.generalSkill}】　 ●　　　　○　　　　○　　　　○　　 　 ○　 ／\n` : ""}${histories.来歴.generalSkill ? `【${histories.来歴.generalSkill}】　 ●　　　　○　　　　○　　　　○　　 　 ○　 ／` : ""}
+
+◆呪文
+呪文行使基準値（知力集中or魂魄集中+技能補正）：【 ${firstStatus.知力点 + secondStatus.集中度} or ${firstStatus.魂魄点 + secondStatus.集中度} 】
+真言：【 0 】　　奇跡：【 0 】　　祖竜：【 0 】　　精霊：【 0 】
+
+◎習得呪文：
+
+◆攻撃
+命中基準値（技量集中）：【 ${firstStatus.技量点 + secondStatus.集中度} 】
+近接：【 0 】　　弩弓：【 0 】　　投擲：【 0 】
+
+◎武器：
+【 () 】
+用途／属性／射程：【  】 命中値合計：【  】 ダメージ：【  】
+効果：
+
+◎効力値
+～14：変化無し　15～19：+1D6　20～24：+2D6　25～29：+3D6　30～39：+4D6　40～：+5D6
+
+◆防御
+回避基準値（技量反射+回避可能な職業LV+技能補正）：【 ${firstStatus.技量点 + secondStatus.反射度}+回避可能な職業LV+技能補正 】
+盾受け基準値（技量反射+盾受け可能な職業LV+技能補正）：【 ${firstStatus.技量点 + secondStatus.反射度}+盾受け可能な職業LV+技能補正 】
+
+◎鎧：
+【 () 】
+属性：【  】　　装甲値：【  】　　回避値補正：【  】　　移動力修正：【  】　　隠密性：【  】
+効果：
+
+◎盾：
+【  】
+属性：【  】　盾受け修正：【  】　　盾受け値：【  】　　隠密性：【  】
+効果：
+
+移動力合計：【 ${status.移動力} 】　　装甲値合計：【  】　　回避値合計：【 ${firstStatus.技量点 + secondStatus.反射度}+回避可能な職業LV+技能補正 】
+盾受け基準値合計：【 ${firstStatus.技量点 + secondStatus.反射度}+盾受け可能な職業LV+技能補正 】　　隠密性合計：【  】
+
+◆所持金
+銀貨：${coin}枚
+
+◆その他の所持品
+冒険者ツール（鈎縄,楔*10,小槌,火口箱,背負い袋,水袋,携帯用食器,白墨,小刀,松明*6）
+携帯食(一日セット)*７、衣類`)
+    }, [race, firstStatus, secondStatus, histories, status, coin]);
+
+    const copySheet = () => {
+        navigator.clipboard.writeText(value)
+            .then(() => {
+                console.log('成功');
+            })
+            .catch(err => {
+                console.error('ユーザが拒否、もしくはなんらかの理由で失敗');
+            });
+    };
+
+    return (
+        <div>
+            <textarea readOnly cols="100" rows="30" value={value}>
+
+            </textarea>
+            <br></br>
+            <button type="button" onClick={copySheet()}>キャラシをクリップボードにコピー</button>
+        </div>
+    );
+}
+
+export default ViewArea;
